4b81eedcbaf494037afae2f31a3ebeb141a953f1
Lucas Moreira <lucas-berto@hotmail.com>
2021-09-13 06:28:12 -0300

    #4 - [Frequency Calculus]

---
+++ Frequency_Calc.py
@@ -0,0 +1,30 @@
+#Frequency calculus
+
+from past.builtins import raw_input
+
+mylist = list(open("C:/Users/Lucas/Documents/Aprendendo/ML Mat/Exercicios-Cap01/arquivos/notas.txt", "r"))
+
+for rec in range(len(mylist)):
+    mylist[rec] = float(mylist[rec])
+
+mylist.sort()
+# print(mylist)
+
+amplitude = max(mylist) - min(mylist)
+
+inputz = 5
+interval = round(amplitude / abs(int(raw_input("digite a quantidade de classes: "))))
+# print(interval)
+
+n = 1
+init = min(mylist)
+mydic = {}
+for rec in range(len(mylist)):
+    if (mylist[rec] >= init) and (mylist[rec] < init + interval):
+        mydic[mylist[rec]] = n
+    else:
+       n += 1
+       init += interval
+       mydic[mylist[rec]] = n
+
+print(mydic)
