7a3a2dec09110f016edd29ce4c54ff42dbc65d26
Lucas Moreira <lucas-berto@hotmail.com>
2021-09-17 07:13:20 -0300

    #6 - [Quadratic Equation]

---
+++ Quadratic_Equation.py
@@ -0,0 +1,20 @@
+import cmath
+import math
+
+
+def quadEquation(a, b, c):
+    r = ((b ** 2) - 4 * a * c)
+
+
+    if r < 0:
+        print(" this equation has no real solution")
+    elif r == 0:
+        x = (-b + math.sqrt(r) / (2 * a))
+        print("this equation has one solution: a%" % (x))
+    else:
+        x = (-b + cmath.sqrt(r)) / (2 * a)
+        x2 = (-b - cmath.sqrt(r)) / (2 * a)
+        print("this equation has two solutions: %a e %a" % (x, x2))
+
+
+quadEquation(1, -1, -12)
